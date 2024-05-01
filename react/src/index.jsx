105313dbb6ba0a07179d86c33bc0941454886468
Veronika Sesay <veronika.sesay@gmail.com>
2024-05-01 22:17:28 +0200

    Node modules updated

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -2,6 +2,16 @@ import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+// const App = () => {
+//   return (
+//     <>
+//       <header></header>
+//       <main>
+//         <HomePage />
+//       </main>
+//       <footer></footer>
+//     </>
+//   );
+// };
+
+createRoot(document.querySelector('#app')).render(<HomePage />);
